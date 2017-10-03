6a3e137c0e65535d7374afe7cd403b67805d0885
Dregassa <dregassa@stuy.edu>
2017-10-02 21:38:43 -0400

    functions complete, perhaps could use better test cases

--- daniel.c
+++ daniel.c
@@ -1,29 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 
 int mystrlen(char* s){
 	int length = 0;
-	while (s + length){
+	while (*(s + length)){
 		length++;
 	}
 	return length;
 }
 
+
 char* mystrcpy(char* dest, char* source){
-	char* end = dest + mystrlen(dest);
-	int lensource = mystrlen(source);
+	int lensource = mystrlen(source); 
 	int i = 0;
-	for(;i < lensource; i++){
-		*(end + i) = *(source + i);	
+	for(;i <= lensource; i++){ // <= adds the null byte
+		*(dest + i) = *(source + i);	
 	}
-	end + i = 0;
 	return dest;
 }
 
+
+
 char* mystrchr(char* s, char c){
 	int i = 0;
-	while (s + i){
-		if (s + i == c){return s+i};
+	while (*(s + i)){
+		if (*(s + i) == c){return s+i;}
 		i++;
 	} 
 	return s+i;
+}
+
+int main(){
+	printf("mystrlen(hello, world) = %d\n", mystrlen("hello, world"));
+	char test[40] = "hello,hello";
+	printf("%s\n", mystrcpy(test,"world"));
+	printf("%s\n", mystrchr("hello, world", 'l'));
 }
