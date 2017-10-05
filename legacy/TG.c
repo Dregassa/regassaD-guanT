dffcc69ecd9975b83276f6931237f54c58cbb79b
TerryMinal <tguan@stuy.edu>
2017-10-04 20:08:36 -0400

    updated legacy files

--- legacy/TG.c
+++ legacy/TG.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-char * strncat2(char * dest, char * src, int n) {
+char * mystrncat(char * dest, char * src, int n) {
   char *a = src;
   char *d2 = dest + strlen(dest);
   int x = 0;
@@ -15,11 +15,11 @@ char * strncat2(char * dest, char * src, int n) {
   return dest;
 }
 
-char * strcat2(char * dest, char * src) {
+char * mystrcat(char * dest, char * src) {
   return strncat(dest, src, strlen(src));
 }
 
-int strcmp2(char * s1, char * s2) {
+int mystrcmp(char * s1, char * s2) {
   char *a = s1;
   char *b = s2;
   while (*a || *b) {
@@ -35,24 +35,24 @@ int strcmp2(char * s1, char * s2) {
 }
 
 int main() {
-  printf("\n------------------testing strncat/strcat----------------\n"); 
+  printf("\n------------------testing strncat/strcat----------------\n");
   char a[] = "main";
-  char src[] = " street"; 
+  char src[] = " street";
   printf("testing strncat: %s\n", strncat(a, src , sizeof(a) - strlen(src) - 1));
   char b[] = "main";
-  printf("testing my own strncat: %s\n", strncat2(b, " street",  sizeof(a) - strlen(src) - 1));
+  printf("testing my own strncat: %s\n", mystrncat(b, " street",  sizeof(a) - strlen(src) - 1));
   char c[]= "main";
   printf("testing strcat: %s\n", strcat(c, " street"));
   char d[]= "main";
-  printf("testing my own strcat: %s\n", strcat2(d, " street"));
+  printf("testing my own strcat: %s\n", mystrcat(d, " street"));
   printf("------------------finished testing strncat/strcat----------------\n\n");
-  printf("------------------testing strcmp----------------\n"); 
-  printf("testing strcmp (comparing hey to hey): %d\n", strcmp("hey", "hey"));  
-  printf("testing my own strcmp (comparing hey to hey): %d\n", strcmp2("hey", "hey"));
-  printf("testing strcmp (comparing Hello to HELLO): %d\n", strcmp("Hello", "HELLO"));  
-  printf("testing strcmp (comparing Hello to HELLO): %d\n", strcmp2("Hello", "HELLO"));
+  printf("------------------testing strcmp----------------\n");
+  printf("testing strcmp (comparing hey to hey): %d\n", strcmp("hey", "hey"));
+  printf("testing my own strcmp (comparing hey to hey): %d\n", mystrcmp("hey", "hey"));
+  printf("testing strcmp (comparing Hello to HELLO): %d\n", strcmp("Hello", "HELLO"));
+  printf("testing strcmp (comparing Hello to HELLO): %d\n", mystrcmp("Hello", "HELLO"));
   printf("testing strcmp (comparing h to \"): %d\n", strcmp("h", ""));
-  printf("testing strcmp (comparing h to \"): %d\n", strcmp2("h", ""));  
+  printf("testing strcmp (comparing h to \"): %d\n", mystrcmp("h", ""));
   printf("------------------finished testing strcmp----------------\n\n");
   return 0;
 }
--- legacy/daniel.c
+++ legacy/daniel.c
@@ -1,6 +1,5 @@
-
 #include <stdio.h>
-#include <mystring.h>
+#include <string.h>
 
 int mystrlen(char* s){
 	int length = 0;
@@ -12,28 +11,48 @@ int mystrlen(char* s){
 
 
 char* mystrcpy(char* dest, char* source){
-	int lensource = mystrlen(source); 
+	int lensource = mystrlen(source);
 	int i = 0;
 	for(;i <= lensource; i++){ // <= adds the null byte
-		*(dest + i) = *(source + i);	
+		*(dest + i) = *(source + i);
 	}
 	return dest;
 }
 
 
 
-char* mystrchr(char* s, char c){
+char* mystrchr(char *s, char c){
 	int i = 0;
 	while (*(s + i)){
-		if (*(s + i) == c){return s+i;}
+		// printf("s: %c\n", *(s+i));
+		if (*(s + i) == c){
+			return s+i;
+		}
 		i++;
-	} 
-	return s+i;
+	}
+	return 0;
 }
 
 int main(){
-	printf("mystrlen(hello, world) = %d\n", mystrlen("hello, world"));
-	char test[40] = "hello,hello";
-	printf("%s\n", mystrcpy(test,"world"));
-	printf("%s\n", mystrchr("hello, world", 'l'));
+	//mystrlen
+	char str[40]="hello";
+	char *str2 = ", world!";
+
+	printf("Testing strlen(str)\n");
+	printf("[standard]: %d\n", (int)strlen(str));
+	printf("[mine]: %d\n", mystrlen(str));
+
+	printf("Testing strcpy(str,str2)\n");
+	printf("[standard]: %s\n", strcpy(str,str2));
+	printf("[mine]: %s\n", mystrcpy(str,str2));
+
+	printf("Testing strchr(str,'h')\n");
+	printf("[standard]: %p\n", strchr(str,'h'));
+	printf("[mine]: %p\n", mystrchr(str,'h'));
+	//
+	//
+	// printf("mystrlen(hello, world) = %d\n", mystrlen("hello, world"));
+	// char test[40] = "hello,hello";
+	// printf("%s\n", mystrcpy(test,"world"));
+	// printf("%s\n", mystrchr("hello, world", 'l'));
 }
